908721d38c2b61dcf69b66f427d65310a1baa3f3
ihksanblny <isanomuzumi878@gmail.com>
2025-05-24 20:13:33 +0700

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <footer className="bg-dark-surface py-6">
       <div className="container mx-auto text-center text-text-muted">
-        <p>&copy; {new Date().getFullYear()} Ihksan. All Rights Reserved.</p>
         <p>Built with ❤️ using React & Tailwind CSS</p>
       </div>
     </footer>
